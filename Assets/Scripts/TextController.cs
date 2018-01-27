d2cb1729c9bb9c9b3b8699c1bbfae2984a8cc147
austin-j-taylor <austin@teamtaylor.org>
2018-01-27 17:07:28 -0500

    Text Updates on screen

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    public GlobalController controller;
+
+    public Text[] textFields;
+
+	// Use this for initialization
+	void Start () {
+        textFields = GetComponentsInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textFields[0].text = "+" + controller.Food;
+        textFields[1].text = "+" + controller.Power;
+        textFields[2].text = "" + controller.Wood;
+        textFields[3].text = "" + controller.NumBuild;
+        textFields[4].text = "" + controller.NumLogger;
+        textFields[5].text = "" + controller.NumElectr;
+        textFields[6].text = "" + controller.NumScav;
+        textFields[7].text = (controller.GetLocation() == null) ? "" : controller.GetLocation().Title;
+    }
+}
