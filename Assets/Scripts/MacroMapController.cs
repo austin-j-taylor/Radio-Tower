6993d843506466bdec624e77770ac8ce17631f1c
austin-j-taylor <austin@teamtaylor.org>
2018-01-27 12:08:02 -0500

    MacroMap swipe on on Tab press; button skeleton

--- Assets/Scripts/MacroMapController.cs
+++ Assets/Scripts/MacroMapController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class MacroMapController : MonoBehaviour {
 
+    private Animator anim;
+    private bool isIn;
+
 	// Use this for initialization
 	void Start () {
-		
+        anim = GetComponent<Animator>();
+        isIn = true;
+        anim.SetBool("IsIn", isIn);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Tab)) {
+            isIn = !isIn;
+            anim.SetBool("IsIn", isIn);
+        }
 	}
 }
