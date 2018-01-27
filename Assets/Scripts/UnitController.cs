76611d05b01c6725900049a7dff5899bae8d0355
IAmNobody4 <qdeggleston@cox.net>
2018-01-27 10:51:51 -0500

    UnitController complies with underscore notation

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitController : MonoBehaviour {
-    private string unitType;
-    private int damageValue, healthValue;
-    private float attackSpeed, speedValue, rangeValue;
-    bool friendly;
+    private string _unitType;
+    private int _damageValue, _healthValue;
+    private float _attackSpeed, _speedValue, _rangeValue;
+    bool _friendly;
 	// Use this for initialization
 	void Start () {
 		
