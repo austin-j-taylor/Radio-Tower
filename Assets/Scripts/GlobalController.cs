6f4b9cf4d426fd1d1066e779f251ebf28695b7e8
IAmNobody4 <qdeggleston@cox.net>
2018-01-26 23:27:36 -0500

    Added Global Controller

---
+++ Assets/Scripts/GlobalController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalController : MonoBehaviour {
+    private int wood, power, food;
+    private int numElectr, numScav, numBuild, numLumber;
+	// Use this for initialization
+	void Start () {
+        wood = 0;
+        power = 0;
+        food = 0;
+        numElectr = 0;
+        numScav = 0;
+        numLumber = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
