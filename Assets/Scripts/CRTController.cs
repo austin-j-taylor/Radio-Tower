5017b1df5263edc5bd11417681194aca691c2256
austin-j-taylor <austin@teamtaylor.org>
2018-01-27 20:27:35 -0500

    Radio Tower dummy button

---
+++ Assets/Scripts/CRTController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CRTController : MonoBehaviour {
+
+    //public Button radioTower;
+    public GameObject towerOrder;
+
+
+	// Use this for initialization
+	void Start () {
+        Button[] buttons = GetComponentsInChildren<Button>();
+
+        //radioTower = buttons[0];
+        //radioTower.onClick.AddListener(OpenTowerOrder);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenTowerOrder() {
+        towerOrder.SetActive(true);
+    }
+
+    public void CancelTowerOrder() {
+        towerOrder.SetActive(false);
+    }
+
+    public void ExecuteTowerOrder() {
+        towerOrder.SetActive(false);
+    }
+}
