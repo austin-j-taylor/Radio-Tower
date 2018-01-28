754265132f728daa814f70621864b45dcae5a2af
austin-j-taylor <austin@teamtaylor.org>
2018-01-28 13:25:34 -0500

    Buttons are disabled when behind map

--- Assets/Scripts/MacroMapController.cs
+++ Assets/Scripts/MacroMapController.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class MacroMapController : MonoBehaviour {
 
     public GlobalController controller;
+    public GameObject cRTScreen;
 
     private Animator anim;
     private LocationButton[] buttons;
-    public Image[] threatStickies;
-    public Text[] threatValues; 
+    private Image[] threatStickies;
+    private Text[] threatValues;
+    private ArrayList microButtons;
     private bool isIn;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         anim = GetComponent<Animator>();
         isIn = false;
         anim.SetBool("IsIn", isIn);
@@ -33,14 +35,20 @@ public class MacroMapController : MonoBehaviour {
             threatStickies[(i - 2) / 2] = allImages[i];
             threatValues[(i - 2) / 2] = threatStickies[(i - 2) / 2].GetComponentInChildren<Text>();
         }
+
+        microButtons = new ArrayList();
+        foreach (Button button in cRTScreen.GetComponentsInChildren<Button>()) {
+            microButtons.Add(button);
+        }
+
     }
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.Tab)) {
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
             isIn = !isIn;
             anim.SetBool("IsIn", isIn);
         }
-        for(int i = 0; i < threatStickies.Length; i++) {
+        for (int i = 0; i < threatStickies.Length; i++) {
             int threat = (int)(controller.Locations[i + 1].ThreatLevel * 100);
             if (threat == 0) {
                 threatStickies[i].enabled = false;
@@ -51,5 +59,11 @@ public class MacroMapController : MonoBehaviour {
                 threatValues[i].text = "THREAT:\n" + threat + "%";
             }
         }
-	}
+    }
+
+    public void SetMicroMapControl(int enable) {
+        foreach (Button button in microButtons) {
+            button.interactable = (enable == 1);
+        }
+    }
 }
