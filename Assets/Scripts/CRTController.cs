3e07517e636f8d0c043cd2cad1dc1bed2f126ae8
austin-j-taylor <austin@teamtaylor.org>
2018-01-27 23:34:08 -0500

    Barricade modifications

--- Assets/Scripts/CRTController.cs
+++ Assets/Scripts/CRTController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CRTController : MonoBehaviour {
-    
+
+    public GlobalController controller;
     public GameObject towerOrder;
     public GameObject barricadeSpawn;
+    public GameObject barricade;
     public Animator towerOrderAnimator;
     public Animator barricadeOrderAnimator;
+    public Button towerButton;
+    public Button barricadeButton;
+    public Canvas barricadesCanvas;
 
-    private Transform[] barricadePositions;
+    public RectTransform[] barricadePositions;
     private bool isInTower;
     private bool isInBarricade;
+    private int currentlyBuilding;
 
 
     // Use this for initialization
     void Start () {
         Button[] buttons = GetComponentsInChildren<Button>();
+        RectTransform[] allTransforms = barricadeSpawn.GetComponentsInChildren<RectTransform>();
+        barricadePositions = new RectTransform[16];
 
-        barricadePositions = barricadeSpawn.GetComponentsInChildren<RectTransform>();
+        for (int i = 1; i < allTransforms.Length; i += 2) {
+            barricadePositions[(i - 1) / 2] = allTransforms[i];
+        }
         
         isInTower = false;
         isInBarricade = false;
@@ -35,6 +45,9 @@ public class CRTController : MonoBehaviour {
     public void OpenTowerOrder() {
         isInTower = true;
         towerOrderAnimator.SetBool("IsIn", isInTower);
+
+        // do resource check, grey out box or un grey out box
+        towerButton.interactable = true;
     }
 
     public void CancelTowerOrder() {
@@ -50,6 +63,11 @@ public class CRTController : MonoBehaviour {
     public void BuildBarricade(int location) {
         isInBarricade = true;
         barricadeOrderAnimator.SetBool("IsIn", isInBarricade);
+        currentlyBuilding = location;
+
+
+        // do resource check, grey out box or un grey out box
+        barricadeButton.interactable = true;
     }
 
     public void CancelBarricadeOrder() {
@@ -60,5 +78,10 @@ public class CRTController : MonoBehaviour {
     public void ExecuteBarricadeOrder() {
         isInBarricade = false;
         barricadeOrderAnimator.SetBool("IsIn", isInBarricade);
+
+        //controller.Wood = controller.Wood - 15;
+        GameObject newBarricade = Instantiate(barricade, barricadePositions[currentlyBuilding].position, Quaternion.identity);
+        //newBarricade.transform.SetParent(barricadesCanvas.transform, true);
+
     }
 }
